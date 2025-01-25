2ce40094985d69c64485fa746adc010936076202
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-26 04:32:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-
 import React from "react";
+// import "./styles.css";
+import Timer from "./Timer";
 import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      {/* <h1>Hello</h1> */}
+      <Timer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
